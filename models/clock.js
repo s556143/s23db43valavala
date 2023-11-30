361bb5b15f4514a166154c183d49ac89e23ef672
s556143 <143040522+s556143@users.noreply.github.com>
2023-11-29 22:24:50 -0600

    pushing value into clock

--- models/clock.js
+++ models/clock.js
@@ -1,7 +1,26 @@
-const mongoose = require("mongoose")
+// const mongoose = require("mongoose")
+// const clockSchema = mongoose.Schema({
+// name: String,
+// price: Number,
+// color: String
+// })
+// module.exports = mongoose.model("clock",clockSchema)
+
+const mongoose = require("mongoose");
 const clockSchema = mongoose.Schema({
-name: String,
-price: Number,
-color: String
-})
-module.exports = mongoose.model("clock",clockSchema)
+    name: {
+        type: String,
+        required: true // "required" instead of "require"
+    },
+    color: {
+        type: String,
+        required: true // "required" instead of "require"
+    },
+    price: {
+        type: Number,
+        min: 10,
+        max: 4800,
+        required: true // "required" instead of "require"
+    }
+});
+module.exports = mongoose.model("clock", clockSchema); 
--- routes/clock.js
+++ routes/clock.js
@@ -1,6 +1,18 @@
 var express = require('express');
 const clock_controlers= require('../controllers/clock');
 var router = express.Router();
+
+// A little function to check if we have an authorized user and continue on
+//or
+// redirect to login.
+const secured = (req, res, next) => {
+if (req.user){
+return next();
+}
+res.redirect("/login");
+}
+
+
 /* GET clock */
 router.get('/', clock_controlers.clock_view_all_Page );
 
